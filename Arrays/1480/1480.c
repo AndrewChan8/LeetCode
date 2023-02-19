085c1be2308f7616da7507b08aad3bfc64e43283
AndrewSushi <AndrewSushi8@gmail.com>
2023-02-19 15:20:26 -0800

    Finished 1480 in C and Python

---
+++ Arrays/1480/1480.c
@@ -0,0 +1,20 @@
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+*/
+
+int *runningSum(int* nums, int numsSize, int* returnSize){
+    *returnSize = numsSize;
+    for(int i = 1; i < numsSize; i++){
+        nums[i] += nums[i-1];
+    }
+    return nums;
+}
+
+int main(){
+
+}
---
+++ Arrays/1480/1480.py
@@ -0,0 +1,16 @@
+"""
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+"""
+
+from typing import List
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
