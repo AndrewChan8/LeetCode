12ca52897238b4326ea6368d5c1f914f4c83b768
AndrewSushi <AndrewSushi8@gmail.com>
2024-01-10 16:25:22 -0800

    Finished JS implementation for 82

--- Linked_Lists/Medium/82/82.js
+++ Linked_Lists/Medium/82/82.js
@@ -10,5 +10,22 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-    
+  if(head === null || head.next === null){
+    return head
+  }
+  let prev = new ListNode
+  prev.next = head;
+  let curr = prev;
+
+  while(curr.next && curr.next.next){
+    if(curr.next.val === curr.next.next.val){
+      let previousVal = curr.next.val
+      while(curr.next && curr.next.val === previousVal){
+        curr.next = curr.next.next
+      }
+    }else{
+      curr = curr.next
+    }
+  }
+  return prev.next
 };
